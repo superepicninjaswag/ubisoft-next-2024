e651ae2ef88455b9f59f4b70e6b05a2247f4a238
Rajvir Samrai <samrai.rajvir@gmail.com>
2024-01-17 18:29:05 -0500

    Create EntityDescriptor class

---
+++ GameTest/Source/ECS/EntityDescriptor.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#include "EntityDescriptor.h"
+
+EntityDescriptor::EntityDescriptor(unsigned short assignedId, unsigned short assignedVersion)
+{
+	id = assignedId;
+	version = assignedVersion;
+}
--- GameTest/Source/ECS/EntityDescriptor.h
+++ GameTest/Source/ECS/EntityDescriptor.h
@@ -1,1 +1,10 @@
 #pragma once
+
+
+struct EntityDescriptor
+{
+	unsigned short id;
+	unsigned short version;
+
+	EntityDescriptor(unsigned short assignedId, unsigned short assignedVersion);
+};
